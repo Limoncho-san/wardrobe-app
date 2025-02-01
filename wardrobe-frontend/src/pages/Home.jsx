f754d264f4238d7c03a8bba339392596306182fc
Georgi Dimov <georgi.dimov01@sap.com>
2025-02-01 23:11:10 +0200

    frontend basic

---
+++ wardrobe-frontend/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+const Home = () => {
+    return (
+      <div className="container">
+        <h1>Welcome to Wardrobe App</h1>
+        <p>Manage your wardrobe reservations with ease.</p>
+      </div>
+    );
+  };
+  
+  export default Home;
+  
---
+++ wardrobe-frontend/src/pages/Reservations.jsx
@@ -0,0 +1,11 @@
+const Reservations = () => {
+    return (
+      <div className="container">
+        <h1>My Reservations</h1>
+        <p>List of all your reservations will be shown here.</p>
+      </div>
+    );
+  };
+  
+  export default Reservations;
+  
